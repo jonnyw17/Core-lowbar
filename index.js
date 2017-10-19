163caadff91b90a010bc64e4870fd34f65e799ef
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-10-19 23:30:44 +0100

    _.values functionality added/fully tested

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ const _ = {};
 
  _.identity = val => val
 
+ _.values = (obj) => {
+   if(typeof obj !== 'object' || obj === null) return [];
+   if(Array.isArray(obj)) return obj;
+   const result = [];
+   for(let key in obj) {
+     result.push(obj[key])
+   }
+   return result
+ }
+
+ _.first = (obj) => {
+   if(Array.isArray(obj) === false) return undefined
+   
+ }
 
 module.exports = _
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -12,7 +12,7 @@ describe('_', function () {
 });
 
 /** ***************IDENTITY*****************/
-describe('#identity', () => {
+describe('_.identity', () => {
     it('is a function', () => {
       expect(_.identity).to.be.a('function');
     });
@@ -27,3 +27,40 @@ describe('#identity', () => {
       expect(_.identity(input) === input).to.equal(true);
     });
   });
+
+/** ***************VALUES*****************/
+ describe.only('_.values', () => {
+   it('is a function', () => {
+     expect(_.values).to.be.a('function');
+   });
+   it('returns empty array if input is not an object', () => {
+     expect(_.values()).to.eql([]);
+     expect(_.values('')).to.eql([]);
+     expect(_.values(1)).to.eql([]);
+   });
+   it('returns empty array if given empty object or empty array', () => {
+     expect(_.values([])).to.eql([]);
+     expect(_.values({})).to.eql([]);
+   });
+   it('return all keys in objects', () => {
+     const input = {'hello': 2};
+     const expected = [2];
+     expect(_.values(input)).to.eql(expected);
+   });
+   it('return all values in objects', () => {
+     const input = {name: 'Jonathan', age: 26 };
+     const expected = ['Jonathan', 26];
+     expect(_.values(input)).to.eql(expected);
+   });
+ });
+
+/** ***************FIRST*****************/
+
+describe('_.first', () => {
+  it('_.first is a function', () => {
+    expect(_.first).to.be.a('function');
+  });
+  it('returns undefined not passed an array with a length greater than 0', () => {
+    expect(_.first()).to.eql();
+  });
+});
